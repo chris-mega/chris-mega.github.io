06993b7cb79a5c32a6d682736681936b6a95fa5c
Chris Melendez <chris.melendezg@gmail.com>
2025-04-07 16:41:41 -0700

    add env vars

--- src/app/components/Portfolio.js
+++ src/app/components/Portfolio.js
@@ -63,7 +63,7 @@ export default function Portfolio() {
     const getPreviews = async () => {
       const prevs = {};
       for (const project of projects) {
-        const response = await fetch(`/api/fetchPreview?url=${encodeURIComponent(project.link)}`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/fetchPreview?url=${encodeURIComponent(project.link)}`);
         const data = await response.json();
         data.tech = project.tech;
         if (project.category in prevs) {
@@ -72,7 +72,6 @@ export default function Portfolio() {
           prevs[project.category] = [data];
         }
       }
-      console.log(prevs);
       setPreviews(prevs);
       setLoading(false);
     }
