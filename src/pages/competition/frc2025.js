909c020b12fe32aa2d459501dc874a784a9ce391
Chris Melendez <chris.melendezg@gmail.com>
2025-04-10 14:59:03 -0700

    update frc

--- src/pages/competition/frc2025.js
+++ src/pages/competition/frc2025.js
@@ -22,13 +22,45 @@ export default function Frc2025() {
         />
         <div className="flex flex-col gap-4">
           <p>
-            After
+            Shortly after moving to British Columbia, I joined{" "}
+            <PostLink href="https://www.zenmakerlab.com/">
+              Zen Maker Lab
+            </PostLink>
+            , a local STEM academy, to teach and mentor multiple programs, one
+            of which was the Robotics Academy. With previous experiences in
+            competitions, now it was my turn to mentor students, something that
+            I have been always wanting to do. Me and other 3 mentors guided 10
+            students in different tasks to build a robot from scratch. The
+            nature of a{" "}
+            <PostLink href="https://www.firstinspires.org/robotics/frc">
+              FIRST Competition
+            </PostLink>{" "}
+            is cooperation, team work and technical challenge.
+          </p>
+          <p>
+            {" "}
+            My role, alongside the other mentors, was to guide their
+            exploration, offering technical insights and helping them navigate
+            the complexities of mechanics, electronics, and coding. We
+            encouraged them to research different mechanisms, understand the
+            game rules, and grasp the fundamentals of robot building and coding.
+            It wasn&apos;t about providing them with the answers, but rather
+            empowering them to discover solutions through experimentation and
+            critical thinking.
+          </p>
+          <p>
+            Here is the{" "}
             <PostLink href="https://zenmakerfrc.weebly.com/">
               Team Website
-            </PostLink>
+            </PostLink>{" "}
+            and{" "}
             <PostLink href="https://frc-events.firstinspires.org/2025/team/9292">
-              Team 9292
+              Team 9292 performance
             </PostLink>
+          </p>
+          <p>
+            Despite the technical challenges and every team member being new to
+            the competition, we placed 20th out of 40 teams
           </p>
         </div>
       </div>
